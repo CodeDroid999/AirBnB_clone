2487cebc4288754355c690efdc4c9b8195b76346
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-12 16:48:47 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
+"""This module creates a Amenity class"""
+
 from models.base_model import BaseModel
 
 
-class User(BaseModel):
-    """Class for managing user objects"""
+class Amenity(BaseModel):
+    """Class for managing amenity objects"""
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    name = ""
