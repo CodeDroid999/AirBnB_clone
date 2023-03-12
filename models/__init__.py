0b1e5346b787000c03ec0f11001be2ee4a8e379e
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-12 16:00:00 +0300

    Delete __init__.py

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-creating a unique FileStorage instance
-for the application.
-"""
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
-storage.reload()
