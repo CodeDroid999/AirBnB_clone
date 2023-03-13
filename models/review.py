cc30eaea14ed8f83149d6a57dd552d362f3731e2
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-13 16:06:13 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""This module creates a Review class"""
-
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Class for managing review objects"""
+    """Represent a review.
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
 
     place_id = ""
     user_id = ""
