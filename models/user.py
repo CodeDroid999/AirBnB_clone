bf770eda1f84b586613bfb82b059f01793a9b5b1
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-13 16:05:13 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
+"""Defines the User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Class for managing user objects"""
+    """Represent a User.
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
 
     email = ""
     password = ""
