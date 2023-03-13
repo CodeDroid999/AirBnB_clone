783a3f9f9a8c664aead8e0e4465056a415bc71c4
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-13 16:06:42 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
-"""This module creates a Place class"""
-
+"""Defines the Place class."""
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Class for managing place objects"""
+    """Represent a place.
+    Attributes:
+        city_id (str): The City id.
+        user_id (str): The User id.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms of the place.
+        number_bathrooms (int): The number of bathrooms of the place.
+        max_guest (int): The maximum number of guests of the place.
+        price_by_night (int): The price by night of the place.
+        latitude (float): The latitude of the place.
+        longitude (float): The longitude of the place.
+        amenity_ids (list): A list of Amenity ids.
+    """
 
     city_id = ""
     user_id = ""
