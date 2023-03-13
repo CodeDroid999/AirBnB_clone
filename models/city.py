2ae7a15952f64de14d733f9a8bcf3e8fdbb755f0
John Mwega <93873207+CodeDroid999@users.noreply.github.com>
2023-03-13 16:07:14 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
-
+"""Defines the City class."""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Class for managing city objects"""
+    """Represent a city.
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
 
     state_id = ""
     name = ""
